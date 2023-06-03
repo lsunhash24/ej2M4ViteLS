6d6d3972f33d24dd921d84bd0cc6445e08d5341c
lsunhash24 <lsanhueza2401@gmail.comm>
2023-06-03 15:31:22 -0400

    ..pruebas

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 			  main: resolve(__dirname, "index.html"),
-			  customPage: resolve(__dirname, "components/proveedores.html"),
+			  customPage: resolve(__dirname, "/components/proveedores.html"),
 			},
 		},	
 	},
