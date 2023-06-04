2686c06f1301a2ac5b86e2dc69c1ae6428392fab
lsunhash24 <lsanhueza2401@gmail.comm>
2023-06-03 23:25:10 -0400

    ..pruebas

--- src/components/proveedor.js
+++ src/components/proveedor.js
@@ -6,6 +6,19 @@ import {
     guardarLocalStorage,
     currencyFormatter
   } from "./funciones.js";
+
+  document.querySelector('#nav').innerHTML = `
+
+<!--Menu de navegación-->
+      <header class="full-width NavBarP container-fluid">
+        <div class="full-width NavBarP-Logo">Ejercicio individual 2 M4</div>
+        <nav class="full-width NavBarP-Nav">
+          <ul class="full-width list-unstyled">
+            <li><a href="./src/components/proveedor.html">Proveedores</a></li>
+            <li><a href="./src/components/articulo.html">Articulos</a></li>
+          </ul>
+        </nav>
+      </header>`
   
   let datos1 = obtenerLocalStorage();
   //mostrar datos del localStorage en una tabla
